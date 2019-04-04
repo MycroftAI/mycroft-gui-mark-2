35ff4c9982496d621c5f287b934db561820be33c
Marco Martin <notmart@gmail.com>
2019-04-04 12:13:47 +0200

    fix rotation config

--- lookandfeel/contents/plasmoidsetupscripts/org.kde.mycroft.mark2.js
+++ lookandfeel/contents/plasmoidsetupscripts/org.kde.mycroft.mark2.js
@@ -1,6 +1,6 @@
 applet.wallpaperPlugin = 'org.kde.color'
 applet.writeConfig("wallpaperPlugin", 'org.kde.color')
-applet.writeConfig("Rotation", "CCW")
+applet.writeConfig("rotation", "CCW")
 applet.currentConfigGroup = ["Wallpaper", "org.kde.color", "General"]
 applet.writeConfig("Color", "0,0,0")
 applet.reloadConfig()
