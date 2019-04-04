74d4b25ddaa062689e7c541b336bcfde73ff8c61
Marco Martin <notmart@gmail.com>
2019-04-04 11:18:22 +0200

    try harder to set color plugin

--- lookandfeel/contents/plasmoidsetupscripts/org.kde.mycroft.mark2.js
+++ lookandfeel/contents/plasmoidsetupscripts/org.kde.mycroft.mark2.js
@@ -1,4 +1,5 @@
 applet.wallpaperPlugin = 'org.kde.color'
+applet.writeConfig("wallpaperPlugin", 'org.kde.color')
 applet.writeConfig("Rotation", "CCW")
 applet.currentConfigGroup = ["Wallpaper", "org.kde.color", "General"]
 applet.writeConfig("Color", "0,0,0")
