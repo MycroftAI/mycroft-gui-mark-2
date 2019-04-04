ff1b8f3ab00d41b56136c93e70ad24a9c11f474c
Marco Martin <notmart@gmail.com>
2019-04-04 16:16:37 +0200

    add back layout.js

--- lookandfeel/contents/plasmoidsetupscripts/org.kde.mycroft.mark2.js
+++ lookandfeel/contents/plasmoidsetupscripts/org.kde.mycroft.mark2.js
@@ -1,5 +1,4 @@
 applet.wallpaperPlugin = 'org.kde.color'
-applet.writeConfig("wallpaperPlugin", 'org.kde.color')
 applet.writeConfig("rotation", "CCW")
 applet.currentConfigGroup = ["Wallpaper", "org.kde.color", "General"]
 applet.writeConfig("Color", "0,0,0")
