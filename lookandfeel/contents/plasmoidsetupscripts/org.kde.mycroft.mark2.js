c5fad5874b62a3e70613e4cf0add2a64676c8c02
Marco Martin <notmart@gmail.com>
2019-04-04 11:16:27 +0200

    correct setup script

--- lookandfeel/contents/plasmoidsetupscripts/org.kde.mycroft.mark2.js
+++ lookandfeel/contents/plasmoidsetupscripts/org.kde.mycroft.mark2.js
@@ -1,7 +1,6 @@
 applet.wallpaperPlugin = 'org.kde.color'
 applet.writeConfig("Rotation", "CCW")
 applet.currentConfigGroup = ["Wallpaper", "org.kde.color", "General"]
-applet.writeConfig("Image", "file:///usr/share/wallpapers/steamos.jpg")
 applet.writeConfig("Color", "0,0,0")
 applet.reloadConfig()
 
